b5794fdd85418fb27c31002b8d97109e01f60494
MattiasErikssonHIS <g10mater@student.his.se>
2015-09-15 22:58:49 +0200

    Style changes made to nav and text

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@
 <head>
 	<div class="page-header">
 		<?php
-			echo "<h1>Prototype</h1>";
+			echo "<h1>Live prototype</h1>";
 		?>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php echo $heading; ?></title>
